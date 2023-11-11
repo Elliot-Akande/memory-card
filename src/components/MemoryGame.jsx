c361175d0f65f3064eb584893b0a0f2be75c303e
Elliot Akande <elbo.lynch@gmail.com>
2023-11-11 20:50:25 +0000

    Create cards function

--- src/components/MemoryGame.jsx
+++ src/components/MemoryGame.jsx
@@ -13,6 +13,22 @@ function MemoryGame() {
       };
     }
   };
+
+  const cards = (numCards) => {
+    const ids = [];
+    // Generate unique pokemon ids in range 1 - 1017
+    for (let i = 0; i < numCards; i++) {
+      let id;
+      do {
+        id = Math.floor(Math.random() * 1017) + 1;
+      } while (ids.includes(id));
+
+      ids.push(id);
+    }
+
+    return ids.map((id) => <Card key={id} id={id} />);
+  };
+
   return (
     <>
       <p>Memory Game Component</p>
