b24f4a1ffe205881f30f08d1e4bbf550a9405266
Elliot Akande <elbo.lynch@gmail.com>
2023-11-11 21:00:22 +0000

    Move fetchPokemon function to Card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,4 +1,17 @@
 function Card() {
+  const fetchPokemon = async (imgId) => {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${imgId}/`);
+    const data = await response.json();
+
+    if (response.ok) {
+      return {
+        id: data.id,
+        name: data.name,
+        img: data.sprites.front_default,
+      };
+    }
+  };
+
   return (
     <>
       <p>This is a card</p>
--- src/components/MemoryGame.jsx
+++ src/components/MemoryGame.jsx
@@ -3,19 +3,6 @@ import Card from "./Card";
 function MemoryGame() {
   const numCards = 12;
 
-  const fetchPokemon = async (imgId) => {
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${imgId}/`);
-    const data = await response.json();
-
-    if (response.ok) {
-      return {
-        id: data.id,
-        name: data.name,
-        img: data.sprites.front_default,
-      };
-    }
-  };
-
   const cards = (numCards) => {
     const ids = [];
     // Generate unique pokemon ids in range 1 - 1017
