710435a4e33c3fddf83903c3ef26b6b995379aba
Elliot Akande <elbo.lynch@gmail.com>
2023-11-11 19:50:33 +0000

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "/src/styles/App.css";
+
+function App() {
+  return (
+    <>
+      <header>Memory Card</header>
+      <main></main>
+    </>
+  );
+}
+
+export default App;
